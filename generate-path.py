aa01e0e18ae310ec55c95fd323d7a2b631e337f5
Sheersha-jain <sheershajain@gmail.com>
2019-10-03 17:53:35 +0530

    graph path

---
+++ generate-path.py
@@ -0,0 +1,22 @@
+graph ={
+    'a':['c'],
+    'b':['d'],
+    'c':['e'],
+    'd':['a', 'd'],
+    'e':['b', 'c']
+}
+
+# function to find path
+def find_path(graph, start, end, path =[]):
+    path = path + [start]
+    if start == end:
+        return path
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                return newpath
+            return None
+
+print(find_path(graph, 'd', 'c'))
+
